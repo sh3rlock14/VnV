bf26acb59b0fa2c27eae946d3d003b1775ae30f7
arl3cchino <mattiacappa@hotmail.it>
2019-04-19 13:15:04 +0200

    del

--- Part 3/includedIn.m
+++
@@ -1,8 +0,0 @@
-function f = includedIn(includee,includer)
-% Returns whether linear space includer includes linear space includee
-
-concat = [includee includer];
-% if rank(concat) == rank(includer) ==> Im(includee) subset Im(includer)
-f = (rank(concat) == rank(includer));
-
-end
